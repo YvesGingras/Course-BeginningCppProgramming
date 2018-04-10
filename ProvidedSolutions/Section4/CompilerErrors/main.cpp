21f2984b126f3b2923dbcfff503386ffa3e3e760
YvesGingras <yves.gingras@icloud.com>
2018-04-10 16:29:51 -0400

    Modification to 'gitignore' and deletion of 'ProvideSolutions' folder.

--- ProvidedSolutions/Section4/CompilerErrors/main.cpp
+++ ProvidedSolutions/Section4/CompilerErrors/main.cpp
@@ -2,7 +2,8 @@
 
 int main() 
 {
-    std::cout << ("Hello world" / 125)  << std::endl;
+    /*std::cout << ("Hello world" / 125)  << std::endl;*/
+    std::cout << ("Hello world")  << std::endl;
     return 0;
 } 
 
