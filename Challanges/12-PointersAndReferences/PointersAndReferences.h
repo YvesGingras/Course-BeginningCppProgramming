4d035ec9ade55c0bed1681bf6fb963aef5e6e15a
YvesGingras <yves.gingras@icloud.com>
2018-04-25 17:12:24 -0400

    12-Pointers and References, 'Challange Solution'.

--- Challanges/12-PointersAndReferences/PointersAndReferences.h
+++ Challanges/12-PointersAndReferences/PointersAndReferences.h
@@ -7,30 +7,28 @@ using namespace std;
 
 namespace PointersAndReferences
 {
-    int *ApplyAll(const int array1[], size_t sizeArray1, const int array2[], size_t sizeArray2) {
-        int *newStorage{nullptr};
+    int *ApplyAll(const int *const array1, size_t sizeArray1, const int *const array2, size_t sizeArray2) {
+        int *newArray{nullptr};
 
-        newStorage = new int[sizeArray1 * sizeArray2];
+        newArray = new int[sizeArray1 * sizeArray2];
 
         size_t counter{0};
         for (size_t i = 0; i < sizeArray2; ++i) {
             for (size_t j = 0; j < sizeArray1 ; ++j) {
-                *(newStorage + counter) = array2[i] * array1[j];
+                *(newArray + counter) = array2[i] * array1[j]; // using 'pointer-offset notation'.
                 ++counter;
             }
         }
 
-        return newStorage;
+        return newArray; //thus, returning the address of the 1st element of the array...
     }
 
-    void PrintContent(const int *content, size_t size) {
-        cout << "The content of the alocated memory storage is :"
-                << "\n[ ";
+    void PrintContent(const int *const content, size_t size) {
+        cout << "\n[ ";
 
-        for (int i = 0; i < size; ++i) {
-            cout << content[i] << " ";
-        }
+        for (size_t i{0}; i < size; ++i)
+            cout << content[i] << " ";         //using 'pointer-subscript notation' **
 
-        cout << "]" << endl;
+        cout << "]\n" << endl;
     }
 }
--- Challanges/12-PointersAndReferences/main.cpp
+++ Challanges/12-PointersAndReferences/main.cpp
@@ -16,11 +16,22 @@ int main() {
     int array1[] {1,2,3,4,5};
     int array2[] {10,20,30};
 
-    size_t sizeArray1 = sizeof(array1)/sizeof(array1[0]);
-    size_t sizeArray2 = sizeof(array2)/sizeof(array2[0]);
+    const size_t sizeArray1 = sizeof(array1)/sizeof(array1[0]);
+    const size_t sizeArray2 = sizeof(array2)/sizeof(array2[0]);
+
+    cout << "Array1: ";
+    PrintContent(array1,sizeArray1);
+
+    cout << "Array2: ";
+    PrintContent(array2,sizeArray2);
+
 
     int *result = ApplyAll(array1, sizeArray1, array2, sizeArray2);
-    PrintContent(result, sizeArray1 * sizeArray2);
+    constexpr size_t resultSize{sizeArray1 * sizeArray2};
+
+    cout << "Result: ";
+    PrintContent(result, resultSize);
+
     delete [] result;
 
     return 0;
