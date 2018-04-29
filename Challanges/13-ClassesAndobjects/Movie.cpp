a6635bfc8ca316c7b1dc0af94da8b67e3fe58377
YvesGingras <yves.gingras@icloud.com>
2018-04-29 09:47:26 -0400

    13-ClassesAndObjects, 'ChallangeSolution; Add CopyConstructor to Movie class'.

--- Challanges/13-ClassesAndobjects/Movie.cpp
+++ Challanges/13-ClassesAndobjects/Movie.cpp
@@ -7,34 +7,12 @@
 
 using namespace std;
 
-int m_watched{0};
-
 Movie::Movie(const string &name, const string &rating, int watched)
         : m_name(name), m_rating(rating), m_watched(watched) {}
 
-const string &Movie::GetName() const {
-    return m_name;
-}
-
-void Movie::SetName(const string &name) {
-    m_name = name;
-}
-
-const string &Movie::GetRating() const {
-    return m_rating;
-}
-
-void Movie::SetRating(const string &rating) {
-    m_rating = rating;
-}
-
-int Movie::GetWatched() const {
-    return m_watched;
-}
-
-void Movie::SetWatched(int watched) {
-    m_watched += watched;
-}
+//copy constructor
+Movie::Movie(const Movie &source)
+        : Movie {source.m_name,source.m_rating,source.m_watched} {}
 
 void Movie::Display() const {
         cout << m_name << ", "
--- Challanges/13-ClassesAndobjects/Movie.h
+++ Challanges/13-ClassesAndobjects/Movie.h
@@ -13,25 +13,24 @@ class Movie
 private:
     std::string m_name;
     std::string m_rating;
-    int m_watched;
+    int m_watched{};
 
 public:
-   Movie(const std::string &name, const std::string &rating, int watched);
+    Movie(const std::string &name, const std::string &rating, int watched);
 
-    const std::string &GetName() const;
+    //copy constructor
+    Movie(const Movie &source);
 
-    void SetName(const std::string &name);
+    const std::string &GetName() const { return m_name;}
+    void SetName(const std::string &name) {this->m_name = name;}
 
-    const std::string &GetRating() const;
+    const std::string &GetRating() const {return m_rating;}
+    void SetRating(const std::string &rating) {this->m_rating = rating;}
 
-    void SetRating(const std::string &rating);
-
-    int GetWatched() const;
-
-    void SetWatched(int watched);
+    int GetWatched() const{return m_watched;}
+    void SetWatched(int watched) {this->m_watched += watched;}
 
     void Display() const;
-
 };
 #endif //CHALLANGES_MOVIE_H
 
