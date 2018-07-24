5c10c43fe0c4fde4b52ba986fd128b0d37779469
YvesGingras <yves.gingras@icloud.com>
2018-07-24 11:56:42 -0400

    20-STL, 'Callenge 2'.

---
+++ Challanges/20-STL/Source/Song.h
@@ -0,0 +1,47 @@
+//
+// Created by Yves Gingras on 18-07-23.
+//
+
+#ifndef INC_20_STL_SONG_H
+#define INC_20_STL_SONG_H
+
+#include <string>
+#include <ostream>
+#include <iomanip>
+
+class Song {
+    friend std::ostream &operator<<(std::ostream &os, const Song &s);
+    std::string name;
+    std::string artist;
+    int rating;
+public:
+    Song() = default;
+    Song(std::string name, std::string artist, int rating)
+            : name{name}, artist{artist}, rating{rating} {}
+    std::string get_name() const {
+        return name;
+    }
+    std::string get_artist() const {
+        return artist;
+    }
+    int get_rating() const {
+        return rating;
+    }
+
+    bool operator<(const Song &rhs) const  {
+        return this->name < rhs.name;
+    }
+
+    bool operator==(const Song &rhs) const  {
+        return this->name == rhs.name;
+    }
+};
+
+std::ostream &operator<<(std::ostream &os, const Song &s) {
+    os << std::setw(20) << std::left << s.name
+       << std::setw(30) << std::left << s.artist
+       << std::setw(2) << std::left << s.rating;
+    return os;
+}
+
+#endif //INC_20_STL_SONG_H
---
+++ Challanges/20-STL/main01Original.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include "Source/Person.h"
+
+using namespace std;
+
+template <typename T>
+void display(const vector<T>& myVector) {
+    cout << "[ ";
+    for (const auto& item : myVector) {
+        cout << item << " ";
+    }
+    cout << "]" << endl;
+}
+
+void display2(const vector<int>& myVector) {
+    cout << "[ ";
+    for_each(myVector.begin(),myVector.end(), [](int x){cout << x << " ";});
+    cout << "]" << endl;
+}
+
+int main() {
+    cout << "\nHello, 20-STL!\n" << endl;
+
+
+
+
+    return 0;
+}
